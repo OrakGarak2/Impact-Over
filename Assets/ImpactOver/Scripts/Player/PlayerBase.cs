634dab2c67c022c921549a2563a6e75dee5951d5
OrakGarak2 <888seonghun888@gmail.com>
2025-07-16 17:34:51 +0900

    추가 | 프로젝트 구조

---
+++ Assets/ImpactOver/Scripts/Player/PlayerBase.cs
@@ -0,0 +1,15 @@
+// System
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Unity
+using UnityEngine;
+
+namespace ImpactOver.Player
+{
+    public class PlayerBase : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/ImpactOver/Scripts/Player/Swordsman/Swordsman.cs
@@ -0,0 +1,29 @@
+// System
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Unity
+using UnityEngine;
+
+// Impact Over
+using ImpactOver.Player;
+
+namespace ImpactOver.Player.Swordsman
+{
+    public class Swordsman : PlayerBase
+    {
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
